35009798c1732a9de8ac5ad861040d3982fd4dc2
Jose Leandro <joseleand06@gmail.com>
2025-10-26 23:03:08 -0300

    8 - OrderStatus enum

--- src/main/java/com/projeto_pessoal/WebService/config/TestConfig.java
+++ src/main/java/com/projeto_pessoal/WebService/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.projeto_pessoal.WebService.config;
 
 import com.projeto_pessoal.WebService.entities.Order;
 import com.projeto_pessoal.WebService.entities.User;
+import com.projeto_pessoal.WebService.entities.enums.OrderStatus;
 import com.projeto_pessoal.WebService.repository.OrderRepository;
 import com.projeto_pessoal.WebService.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +26,14 @@ public class TestConfig implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Teste Config, P03 - WebService!");
+        System.out.println("Test Config, P03 - WebService!");
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456", null);
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456", null);
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
--- src/main/java/com/projeto_pessoal/WebService/entities/Order.java
+++ src/main/java/com/projeto_pessoal/WebService/entities/Order.java
@@ -1,6 +1,7 @@
 package com.projeto_pessoal.WebService.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projeto_pessoal.WebService.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -27,9 +28,18 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -41,5 +51,15 @@ public class Order implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hashCode(getId());
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 }
