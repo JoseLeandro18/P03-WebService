f593d34c0632f2b9e5a01afafb6ef17cbc457e58
Jose Leandro <joseleand06@gmail.com>
2025-11-26 21:41:38 -0300

    User entity and resource

--- src/main/java/com/projetopessoal/p03webservice/P03webserviceApplication.java
+++ src/main/java/com/projetopessoal/p03webservice/P03webserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class P03webserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(P03webserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(P03webserviceApplication.class, args);
+    }
 
 }
--- src/test/java/com/projetopessoal/p03webservice/P03webserviceApplicationTests.java
+++ src/test/java/com/projetopessoal/p03webservice/P03webserviceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class P03webserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
