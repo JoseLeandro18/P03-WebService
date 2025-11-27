3a673caf9fa18bbd33eaa123576756394d74dd6f
Jose Leandro <joseleand06@gmail.com>
2025-11-27 17:22:56 -0300

    OrderStatus enum

--- src/main/java/com/projetopessoal/p03webservice/entities/Order.java
+++ src/main/java/com/projetopessoal/p03webservice/entities/Order.java
@@ -3,6 +3,7 @@ package com.projetopessoal.p03webservice.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.projetopessoal.p03webservice.entities.enums.OrderStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +22,8 @@ public class Order implements Serializable {
     private Long id;
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -28,10 +31,11 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -49,6 +53,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
